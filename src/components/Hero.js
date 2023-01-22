1309db964bb99289c63c286938b26803a4cd130d
Gr34sy <slupek3@poczta.fm>
2023-01-22 19:10:39 +0100

    added hero pic responsiveness and inset shadow

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,12 @@
 import React from "react";
+import masterpiece from "../assets/masterpiece.png";
 
 export function Hero(){
     return(
         <main className="hero">
-           <div className="hero__img">
-
-           </div>
+            <figure className="hero__container">
+                <img src={masterpiece} className="hero__img"/>
+            </figure>
         </main>
     )
 }
