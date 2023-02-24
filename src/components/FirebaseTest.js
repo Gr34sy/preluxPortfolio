309dcaa850b7c2f1592bb9bdfdf81311b3b3e18d
Gr34sy <slupek3@poczta.fm>
2023-02-24 14:31:31 +0100

    added firestorage reference list function

--- src/components/FirebaseTest.js
+++ src/components/FirebaseTest.js
@@ -1,5 +1,27 @@
 import React from "react";
-import "../firebase";
+import firebaseApp from "../firebase";
+import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
+
+const storage = getStorage(firebaseApp);
+const listRef = ref(storage, 'images');
+
+const imagesRefs = [];
+
+listAll(listRef)
+.then((res) => {
+  res.items.forEach((itemRef) => {
+    imagesRefs.push(itemRef);
+  });
+})
+.then( () => {
+    console.log(imagesRefs);
+})
+.catch((error) => {
+  console.error(error);
+});
+
+
+
 
 export function FirebaseTest(){
     return(
