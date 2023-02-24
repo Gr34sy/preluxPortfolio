16893b0efb79ec7864f8786bba72ff09e2b14d56
Gr34sy <slupek3@poczta.fm>
2023-02-24 16:25:23 +0100

    changed gitignore

--- src/components/FirebaseTest.js
+++ src/components/FirebaseTest.js
@@ -1,32 +1,37 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
 import firebaseApp from "../firebase";
 import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
 
-const storage = getStorage(firebaseApp);
-const listRef = ref(storage, 'images');
-
-const imagesRefs = [];
-
-listAll(listRef)
-.then((res) => {
-  res.items.forEach((itemRef) => {
-    imagesRefs.push(itemRef);
-  });
-})
-.then( () => {
-    console.log(imagesRefs);
-})
-.catch((error) => {
-  console.error(error);
-});
-
-
-
-
 export function FirebaseTest(){
+const [imagesRefs, setImagesRefs] = useState([]);
+
+useEffect(() => {
+    const storage = getStorage(firebaseApp);
+    const listRef = ref(storage, 'images');
+
+    listAll(listRef)
+    .then((res) => {
+        res.items.forEach((itemRef) => {
+            setImagesRefs([ ...imagesRefs, itemRef]);
+        })
+    })
+    .then(() => {
+        console.log(imagesRefs);
+    })
+    .catch((error) => {
+        console.error(error);
+    })
+}, []);
+
     return(
         <section style = {{backgroundColor: 'white'}}>
-            <img src='' alt='firebase image'/>
+            {imagesRefs.map(
+                (imageRef) => {
+                    return <img src={imageRef} alt="Digital Art"/>;
+                }
+            )}
         </section>
     )
 }
