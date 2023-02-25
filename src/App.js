ee1c8bfe41d68f04df8ca046edfc3954b1aafe61
Gr34sy <slupek3@poczta.fm>
2023-02-25 12:25:13 +0100

    added boxshadow to navbar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './scss/main.scss';
 
 import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
+import { AboutMe } from './components/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Hero/>
+      <AboutMe/>
     </div>
   );
 }
