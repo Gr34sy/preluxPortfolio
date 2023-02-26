d1b4ad88e0d5651bd12c7d789445dc260cf3c11e
Gr34sy <slupek3@poczta.fm>
2023-02-26 12:30:07 +0100

    finished aboutme section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,14 +4,14 @@ import { FirebaseImage } from './FirebaseImage';
 export function AboutMe(){
     return(
         <section className="about" id="about">
-            <article className="about__first">
+            <article className="about__section about__first">
 
-                <figure className="about__first-left">
+                <figure className="about__left">
                     <FirebaseImage imgNum='2' imgClass="about__img" format="jpg" />
                     <FirebaseImage imgNum='3' imgClass="about__img" format="jpg" />
                 </figure>
                 
-                <div className="about__first-right">
+                <div className="about__right">
                     <h1 className="about__title">About me</h1>
                     <p className="about__text">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
@@ -28,10 +28,28 @@ export function AboutMe(){
 
             </article>
 
-        
-            
-            <article className="about__second">
+            <article className="about__section about__second">
+
+                <figure className="about__left">
+                    <FirebaseImage imgNum='4' imgClass="about__img" format="jpg" />
+                    <FirebaseImage imgNum='5' imgClass="about__img" format="png" />
+                </figure>
                 
+                <div className="about__right">
+                    <h2 className="about__title">My passions</h2>
+                    <p className="about__text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                    </p>
+
+                    <p className="about__text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                    </p>
+
+                    <p className="about__text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                    </p>
+                </div>
+
             </article>
         </section>
     )
