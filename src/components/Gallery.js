d7ee5cb502d6400e1ad92e41c0069d102eb0d1ed
Gr34sy <slupek3@poczta.fm>
2023-03-02 11:25:52 +0100

    added gallery component and scss file

---
+++ src/components/Gallery.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { FirebaseImage } from './FirebaseImage';
+
+export function Gallery(){
+    return(
+        <section className="gallery">
+            <div className="gallery__container">
+                <figure className="gallery__item">
+
+                </figure>
+            </div>
+        </section>
+    )
+}
