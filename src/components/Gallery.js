86ebb0017ec712504739f98dcf6cb717e75d9cd1
Gr34sy <slupek3@poczta.fm>
2023-03-02 12:31:09 +0100

    added gallery items on page

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -10,6 +10,11 @@ export function Gallery(){
                 <GalleryItem imgNum='3'  format="jpg"/>
                 <GalleryItem imgNum='4'  format="jpg"/>
                 <GalleryItem imgNum='5'  format="png"/>
+                <GalleryItem imgNum='1'  format="png"/>
+                <GalleryItem imgNum='2'  format="jpg"/>
+                <GalleryItem imgNum='3'  format="jpg"/>
+                <GalleryItem imgNum='4'  format="jpg"/>
+                <GalleryItem imgNum='5'  format="png"/>
             </div>
         </section>
     )
