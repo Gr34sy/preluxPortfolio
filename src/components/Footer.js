5256168187333568433e7ec9bf4e36748eedab1a
Gr34sy <slupek3@poczta.fm>
2023-03-06 18:24:17 +0100

    changed nav bg and section colors

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons' 
+
+export function Gallery(){
+    return(
+        
+        <footer className="footer">
+            <p>Find me on Instagram to see more of my artworks!</p>
+            <a href="">
+                <FontAwesomeIcon icon={faInstagram} />
+            </a>
+        </footer>
+        
+    )
+}
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { GalleryItem } from './GalleryItem';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons' 
-
 export function Gallery(){
     return(
         <section className="gallery" id="gallery">
@@ -19,12 +16,6 @@ export function Gallery(){
                 <GalleryItem imgNum='3'  format="jpg"/>
                 <GalleryItem imgNum='4'  format="jpg"/>
                 <GalleryItem imgNum='5'  format="png"/>
-            </div>
-            <div className="gallery__more-art">
-                <p>Find me on Instagram to see more of my artworks!</p>
-                <a href="">
-                    <FontAwesomeIcon icon={faInstagram} />
-                </a>
             </div>
         </section>
     )
